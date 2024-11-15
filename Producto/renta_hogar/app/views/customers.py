9166c37673b03b01c68db91b67296775aebd9b56
rafduqcol <rafduqcol@alum.us.es>
2024-11-15 18:06:51 +0100

    feat: Cambio de idioma

--- Producto/renta_hogar/app/views/customers.py
+++ Producto/renta_hogar/app/views/customers.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from app.models import Apartment    
 from django.contrib.auth.decorators import login_required
 from app.models import Reservation
 
 def customer_menu(request):
-    return render(request, 'customer/customer_menu.html')
+    price_min = request.GET.get('precio_min')
+    price_max = request.GET.get('precio_max')
+    huespedes = request.GET.get('huespedes')
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    apartments = Apartment.objects.all()
+
+    if price_min:
+        apartments = apartments.filter(price__gte=price_min)
+    if price_max:
+        apartments = apartments.filter(price__lte=price_max)
+    if huespedes:
+        apartments = apartments.filter(guest_count=huespedes)
+    if fecha_inicio and fecha_fin:
+        apartments = apartments.exclude(
+            availabilities__start_date__lt=fecha_fin,
+            availabilities__end_date__gt=fecha_inicio
+        )
+
+
+    return render(request, 'customer/customer_menu.html', {
+        'apartments': apartments,
+        'request': request  
+    })
 
 @login_required
 def manage_reservations(request):
