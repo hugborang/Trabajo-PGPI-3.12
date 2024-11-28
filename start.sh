bccf8e79ea971bd2d960c48b3e8d2cbab32e1c67
hugborang <hugo.bo.an@gmail.com>
2024-11-28 16:53:03 +0100

    feat: Añadido script de inicializacion

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Mover al directorio del proyecto si es necesario
+cd Producto/renta_hogar
+
+# Ejecutar migraciones
+python manage.py makemigrations
+python manage.py migrate
+
+# Cargar datos iniciales
+python manage.py loaddata populate_data.json
+
+# Iniciar el servidor Django
+python manage.py runserver 0.0.0.0:$PORT
